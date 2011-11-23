b4e7721268c71772e1930b21375c6482cc021581
matteosister <matteog@gmail.com>
2011-11-23 18:04:31 +0100

    dummy-commit

--- Resources/compass/config.rb
+++ Resources/compass/config.rb
@@ -2,16 +2,18 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../../../../../web/css"
+css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "../bundles/cypressgitelephant"
+images_dir = "../../../../../web/bundles/cypressgitelephant/images"
 javascripts_dir = "javascripts"
+http_images_path = "/bundles/cypressgitelephant/images"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
