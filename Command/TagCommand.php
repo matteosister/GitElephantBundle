79830b4e2ab86cd093ca7e49fff0491e85f5709e
David Romaní <david@flux.cat>
2014-09-26 16:24:55 +0200

    add empty repository exception

--- Command/TagCommand.php
+++ Command/TagCommand.php
@@ -2,6 +2,8 @@
 
 namespace Cypress\GitElephantBundle\Command;
 
+use Cypress\GitElephantBundle\Collection\GitElephantRepositoryCollection;
+use GitElephant\Repository;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -58,19 +60,32 @@ EOT
      *
      * @param InputInterface  $input
      * @param OutputInterface $output
-     * 
+     *
+     * @throws \Exception
      * @return int|null|void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // Welcome
         $output->writeln(
-            '<info>Welcome to the Cypress GitElephantBundle Tag command.</info>'
+            '<info>Welcome to the Cypress GitElephantBundle tag command.</info>'
         );
         if ($input->getOption('no-push')) {
             $output->writeln(
                 '<comment>--no-push option enabled (this option disable push tag to remote repository)</comment>'
             );
+        }
+
+        /** @var GitElephantRepositoryCollection $rc */
+        $rc = $this->getContainer()->get('git_repositories');
+
+        if ($rc->count() == 0) {
+            throw new \Exception('Must have at least one Git repository. See https://github.com/matteosister/GitElephantBundle#how-to-use');
+        }
+
+        /** @var Repository $repository */
+        foreach ($rc as $repository) {
+            $output->writeln($repository->getName());
         }
     }
 }
